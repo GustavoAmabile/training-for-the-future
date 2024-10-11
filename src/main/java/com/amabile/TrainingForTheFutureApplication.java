6e80cda1c8df0d71ea34b5e18020be8d53553452
Gustavo Amabile <gustavoamabile@outlook.com>
2024-10-11 16:45:32 -0300

    Initial commit

---
+++ src/main/java/com/amabile/TrainingForTheFutureApplication.java
@@ -0,0 +1,13 @@
+package com.amabile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingForTheFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingForTheFutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amabile/TrainingForTheFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amabile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingForTheFutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
